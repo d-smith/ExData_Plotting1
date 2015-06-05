efa3c0cbda4f2de1b1ed34c6899bc5c7d8f85cb5
d-smith <doug.smith.mail@gmail.com>
2015-06-05 13:03:50 -0600

    Added plot 4, mods to earlier plots

--- plot4.R
+++ plot4.R
@@ -1,13 +1,31 @@
-generatePlot3 <- function() {
+generatePlot4 <- function() {
+
+  ## Load data and open device
   source("./loaddataset.R")
   getData()
   data <- loadAndFilterData()
-  png(filename="./plot3.png", width=480, height=480)
+  png(filename="./plot4.png", width=480, height=480)
   
+  ## 2x2 layout
+  par(mfrow=c(2,2))
+  
+  ## 1st plot
+  with(data, plot(DateTime,Global_active_power,type="l",ylab="Global Active Power",xlab=""))
+  
+  ## 2nd plot
+  with(data, plot(DateTime,Voltage, type="l", xlab="datetime"))
+  
+  ## 3rd plot
   with(data, plot(DateTime, Sub_metering_1, type = "n", ylab="Energy sub metering",xlab=""))
   with(data, lines(DateTime, Sub_metering_1, col = "black"))
   with(data, lines(DateTime, Sub_metering_2, col = "red"))
   with(data, lines(DateTime, Sub_metering_3, col = "blue"))
   legend("topright",col=c("black","red","blue"),lty=c(1,1,1),legend=c("Sub_metering_1","Sub_metering_2","Sub_metering_3"))
+  
+  ## 4th plot
+  with(data, plot(DateTime,Global_reactive_power,type="l", xlab="datetime"))
+  
+  ## Close device
   dev.off()
+  
 }
