6d71c86a4de2b21fe523e1295e4f57bbc278a498
d-smith <doug.smith.mail@gmail.com>
2015-06-06 12:28:48 -0600

    Added commentary to loaddataset.R

--- loaddataset.R
+++ loaddataset.R
@@ -1,8 +1,10 @@
+## Downlod the dataset zip file the plots are created from.
 downloadZip <- function() {
   fileUrl <- "https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2Fhousehold_power_consumption.zip"
   download.file(fileUrl, destfile="./dataset.zip", method="curl")
 }
 
+## Check to see if the data file exists, if not download and unzip it.
 getData <- function() {
   if(!file.exists("./household_power_consumption.txt")) {
     downloadZip()
@@ -10,6 +12,7 @@ getData <- function() {
   }
 }
 
+## Load the data, subset it, and add a combined data/time column for plotting.
 loadAndFilterData <- function() {
   data <- read.csv("./household_power_consumption.txt",sep=";",header=TRUE,stringsAsFactors=FALSE,colClasses=c("character","character","numeric","numeric","numeric","numeric","numeric","numeric","numeric"),na.strings=c("?"))
   data$Date <- as.Date(data$Date,"%d/%m/%Y")
